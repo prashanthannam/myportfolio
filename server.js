aaa498bf4edfb5dc4bfae524f1217834e462845f
prashanth <prashanth31399@gmail.com>
2020-08-31 16:56:49 +0530

    Adding files.

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
-app.use(history());
 
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
